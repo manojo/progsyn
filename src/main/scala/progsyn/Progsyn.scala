ba14684fad004bc6b92beaa6bf0697866185d88d
manojo <manojo10386@gmail.com>
2017-06-15 20:17:55 +0530

    refactor notify to backPropagate, pull it into traversal

--- src/main/scala/progsyn/Progsyn.scala
+++ src/main/scala/progsyn/Progsyn.scala
@@ -61,12 +61,6 @@ object ProgSyn {
     var parent: Option[Node[T]]
     /** Indicates if this is the left, right or down child of the parent */
     val dir: Direction
-
-    def notify(
-      ts: Stream[T],
-      queue: Queue[Node[T]],
-      k: Queue[Node[T]] => Stream[T],
-      dir: Direction): Stream[T]
   }
 
   object Node {
@@ -88,78 +82,18 @@ object ProgSyn {
 
     val leftSolutions: ListBuffer[T] = ListBuffer.empty[T]
     val rightSolutions: ListBuffer[T] = ListBuffer.empty[T]
-
-    def notify(
-        ts: Stream[T],
-        queue: Queue[Node[T]],
-        k: Queue[Node[T]] => Stream[T],
-        dir: Direction): Stream[T] = {
-
-      /**
-       * based on where the notification came from
-       * compute possible resulting nodes
-       * Also, as a side effect, left or right solutions is updated
-       */
-      val newSolutions = (dir match {
-        case Left => leftSolutions ++= ts; for (l <- ts; r <- rightSolutions) yield builder(l, r)
-        case Right => rightSolutions ++= ts; for (l <- leftSolutions; r <- ts) yield builder(l, r)
-        case _ => ??? //An AndNode should not have a Down child
-      }).toStream
-
-      parent match {
-
-        /**
-         * Since no parent node, all I need to do is to cross product
-         * these solutions with those on the other side, and ship
-         * the results out
-         * ATTENTION! cannot use `++` because it is inherited from eager collections
-         * use `#:::` for preserving the laziness aspect
-         */
-        case None => newSolutions #::: k(queue)
-        case Some(p) => p.notify(newSolutions, queue, k, this.dir)
-      }
-
-    }
   }
 
   case class OrNode[T](
-      var parent: Option[Node[T]],
-      dir: Direction,
-      var leftChild: Node[T],
-      rightChild: Node[T]) extends Node[T] {
-
-    def notify(
-      ts: Stream[T],
-      queue: Queue[Node[T]],
-      k: Queue[Node[T]] => Stream[T],
-      dir: Direction): Stream[T] = parent match {
-
-      /**
-       * if I have no parent, I can output the nodes forwarded to me
-       * ATTENTION! cannot use `++` because it is inherited from eager collections
-       * use `#:::` for preserving the laziness aspect
-       */
-      case None => ts #::: k(queue)
-      case Some(p) => p.notify(ts, queue, k, this.dir)
-    }
-  }
+    var parent: Option[Node[T]],
+    dir: Direction,
+    var leftChild: Node[T],
+    rightChild: Node[T]) extends Node[T]
 
   case class Terminal[T](
-      value: T,
-      var parent: Option[Node[T]],
-      dir: Direction) extends Node[T] {
-
-    /**
-     * Notify should never be triggered
-     * for a terminal
-     */
-    def notify(
-      ts: Stream[T],
-      queue: Queue[Node[T]],
-      k: Queue[Node[T]] => Stream[T],
-      dir: Direction): Stream[T] = ???
-
-  }
+    value: T,
+    var parent: Option[Node[T]],
+    dir: Direction) extends Node[T]
 
   def bfTraverse[T](rootNode: Node[T]): Stream[T] = {
     import Node._
@@ -179,23 +113,70 @@ object ProgSyn {
           case AndNode(p, d, l, r, _) =>
             val newL = copy(l, p = Some(hd), d = Left)
             val newR = copy(r, p = Some(hd), d = Right)
-            assert(!(newL eq l))
-            assert(!(newR eq r))
             loop(tl :+ newL :+ newR)
 
           case OrNode(p, d, l, r)  =>
             val newL = copy(l, p = Some(hd), d = Left)
             val newR = copy(r, p = Some(hd), d = Right)
-            assert(!(newL eq l))
-            assert(!(newR eq r))
             loop(tl :+ newL :+ newR)
 
           case Terminal(v, p, d)  => p match {
-            case Some(parent) => parent.notify(v #:: Stream.empty, tl, loop _, d)
+            case Some(parent) => backPropagate(parent, v #:: Stream.empty, tl, d)
             case None         => v #:: loop(tl)
           }
         }
       }
+    }
+
+    def backPropagate(
+        node: Node[T],
+        ts: Stream[T],
+        queue: Queue[Node[T]],
+        dir: Direction): Stream[T] = node match {
+      
+      case andNode @ AndNode(parent, d, _, _, f) => {
+        /**
+         * based on where the notification came from
+         * compute possible resulting nodes
+         * Also, as a side effect, left or right solutions is updated
+         */
+        val newSolutions = (dir match {
+          case Left => 
+            andNode.leftSolutions ++= ts
+            for (l <- ts; r <- andNode.rightSolutions) yield f(l, r)
+          
+          case Right => 
+            andNode.rightSolutions ++= ts
+            for (l <- andNode.leftSolutions; r <- ts) yield f(l, r)
+          
+          case _ => sys.error("AndNode can't have a Down Child")
+        }).toStream
+
+        parent match {
+          /**
+           * Since no parent node, all I need to do is to cross product
+           * these solutions with those on the other side, and ship
+           * the results out
+           * ATTENTION! cannot use `++` because it is inherited from eager collections
+           * use `#:::` for preserving the laziness aspect
+           */
+          case None => newSolutions #::: loop(queue)
+          case Some(p) => backPropagate(p, newSolutions, queue, d)
+        }
+      }
+        
+      /**
+       * if I have no parent, I can output the nodes forwarded to me
+       * ATTENTION! cannot use `++` because it is inherited from eager collections
+       * use `#:::` for preserving the laziness aspect
+       */
+      case OrNode(parent, d, _, _) => parent match {
+        case None => ts #::: loop(queue)
+        case Some(p) => backPropagate(p, ts, queue, d)
+      }
+      
+      case Terminal(v, p, d) => 
+        sys.error("can't back propagate to a Terminal")
     }
 
     loop(Queue(rootNode))
