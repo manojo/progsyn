a6adf2db323c681b046182233636ba236dcc2e1d
manojo <manojo10386@gmail.com>
2017-06-22 16:22:08 +0530

    wip

--- src/main/scala/progsyn/Progsyn.scala
+++ src/main/scala/progsyn/Progsyn.scala
@@ -2,30 +2,53 @@ package progsyn
 
 import traversal._
 import graph._
+import grammar._
 
-object ProgSyn extends Traversal {
+/**
+ * Defining the add language
+ */
+trait AddLanguage extends Grammars {
+
+  type Input
+  def input: Input
+
+  type Output
+
+  abstract class Exp
+  case class Add(l: Exp, r: Exp) extends Exp
+  case class Lookup(i: Int) extends Exp
+
+  def s: Grammar[Exp] = (t ~ s).map { case (l, r) => Add(l, r) } | t
+  def t: Grammar[Exp] = natGen map Lookup
+
+  def eval(e: Exp): Output
+
+}
+
+object ProgSyn extends Traversal with AddLanguage {
+
+  type Input = Array[Int]
+  def input = Array(1, 2, 3)
+
+  type Output = Int
+
+  def eval(e: Exp): Int = e match {
+    case Add(l, r) => eval(l) + eval(r)
+    case Lookup(i) => input(i)
+  }
+
   def main(args: Array[String]): Unit = {
     println("Hi!")
-
-    abstract class Exp
-    case class Add(l: Exp, r: Exp) extends Exp
-    case object Lookup extends Exp
 
     /**
      * s := (t ~ s) map f | t
      * t := Lookup
      */
-
-    val t: Node[Exp] = Terminal(Lookup, None, Right)
+    val t: Node[Exp] = Terminal(Lookup(1), None, Right)
     val rootNode: Node[Exp] = OrNode(None, Down, null, t)
     val andNode: Node[Exp] = AndNode(None, Left, t, rootNode, (l, r) => Add(l, r))
     rootNode match { case o: OrNode[_] => o.leftChild = andNode }
 
-
     println(bfTraverse(rootNode).take(4).toList)
-    //println("prioritytraverse now")
-    //priorityTraverse(nodes).foreach(x => println(x))
-    //println(from(3))
   }
 }
-
--- src/main/scala/traversal/Traversal.scala
+++ src/main/scala/traversal/Traversal.scala
@@ -4,45 +4,49 @@ import graph._
 import scala.collection.immutable.Queue
 
 trait Traversal {
-  def bfTraverse[T](rootNode: Node[T]): Stream[T] = {
-    import Node._
+  import Node._
 
-    /**
-     * The inner loop doing a breadth-first traversal over a node
-     * structure
-     * Since we are generating a grammar, we will always add new children
-     * for every node we see, setting their parent the node we just have observed
-     * This will ensure that the right parent is preserved at all stages
-     */
-    def loop(ls: Queue[Node[T]]): Stream[T] = {
-      if (ls.isEmpty) Stream.empty
-      else {
-        val (hd, tl) = ls.dequeue
-        hd match {
-          case AndNode(p, d, l, r, _) =>
-            val newL = copy(l, p = Some(hd), d = Left)
-            val newR = copy(r, p = Some(hd), d = Right)
-            loop(tl :+ newL :+ newR)
+  /**
+   * The inner loop doing a breadth-first traversal over a node
+   * structure
+   * Since we are generating a grammar, we will always add new children
+   * for every node we see, setting their parent the node we just have observed
+   * This will ensure that the right parent is preserved at all stages
+   */
+  private def loop[T](ls: Queue[Node[T]]): Stream[T] = {
+    if (ls.isEmpty) Stream.empty
+    else {
+      val (hd, tl) = ls.dequeue
+      hd match {
+        case AndNode(p, d, l, r, _) =>
+          val newL = copy(l, p = Some(hd), d = Left)
+          val newR = copy(r, p = Some(hd), d = Right)
+          loop(tl :+ newL :+ newR)
 
-          case OrNode(p, d, l, r)  =>
-            val newL = copy(l, p = Some(hd), d = Left)
-            val newR = copy(r, p = Some(hd), d = Right)
-            loop(tl :+ newL :+ newR)
+        case OrNode(p, d, l, r)  =>
+          val newL = copy(l, p = Some(hd), d = Left)
+          val newR = copy(r, p = Some(hd), d = Right)
+          loop(tl :+ newL :+ newR)
 
-          case Terminal(v, p, d)  => p match {
-            case Some(parent) => backPropagate(parent, v #:: Stream.empty, tl, d)
-            case None         => v #:: loop(tl)
-          }
-        }
+        case Terminal(v, p, d)  =>
+          backPropagate(p, v #:: Stream.empty, tl, d)
       }
     }
+  }
 
-    def backPropagate(
-        node: Node[T],
-        ts: Stream[T],
-        queue: Queue[Node[T]],
-        dir: Direction): Stream[T] = node match {
+  /**
+   * Node `node` receives some solutions `ts` from a child node
+   * at direction `dir`. If `node` is `None`, we can't go higher up
+   * and we just output solutions here. Otherwise we propagate a level
+   * higher
+   */
+  def backPropagate[T](
+      receiver: Option[Node[T]],
+      ts: Stream[T],
+      queue: Queue[Node[T]],
+      dir: Direction): Stream[T] = receiver match {
 
+    case Some(node) => node match {
       case andNode @ AndNode(parent, d, _, _, f) => {
         /**
          * based on where the notification came from
@@ -61,17 +65,7 @@ trait Traversal {
           case _ => sys.error("AndNode can't have a Down Child")
         }).toStream
 
-        parent match {
-          /**
-           * Since no parent node, all I need to do is to cross product
-           * these solutions with those on the other side, and ship
-           * the results out
-           * ATTENTION! cannot use `++` because it is inherited from eager collections
-           * use `#:::` for preserving the laziness aspect
-           */
-          case None => newSolutions #::: loop(queue)
-          case Some(p) => backPropagate(p, newSolutions, queue, d)
-        }
+        backPropagate(parent, newSolutions, queue, d)
       }
 
       /**
@@ -79,15 +73,45 @@ trait Traversal {
        * ATTENTION! cannot use `++` because it is inherited from eager collections
        * use `#:::` for preserving the laziness aspect
        */
-      case OrNode(parent, d, _, _) => parent match {
-        case None => ts #::: loop(queue)
-        case Some(p) => backPropagate(p, ts, queue, d)
-      }
-
-      case Terminal(v, p, d) =>
-        sys.error("can't back propagate to a Terminal")
+      case OrNode(parent, d, _, _) => backPropagate(parent, ts, queue, d)
+      case t: Terminal[_] =>
+        sys.error("a terminal cannot be a receiver in backprop")
     }
 
-    loop(Queue(rootNode))
+    case None => ts #::: loop(queue)
+  }
+
+  def bfTraverse[T](rootNode: Node[T]): Stream[T] = loop(Queue(rootNode))
+}
+
+/**
+ * def loop(ls: Queue[Node[T]]): Stream[T] = {
+  if (ls.isEmpty) Stream.empty
+  else {
+    val (hd, tl) = ls.dequeue
+    hd match {
+      case t: Terminal => backPropagate(t.parent, t.values, tl, t.pos)
+      case _           =>
+        val children = hd.children.copyWith(...)
+        loop(tl ++ children)
+    }
   }
 }
+
+def backPropagate[T](
+    node: Node[_],
+    values: List[T],
+    rest: Queue[Node[_]],
+    pos: Int): Stream[_] = node match {
+
+  case a: AndNode =>
+    val newSolutions = a.storeAndGetNewSols(values, pos)
+  case _ =>
+
+  node.parent match {
+    case None => newSolutions #::: loop(queue)
+    case Some(p) => backPropagate(p, newSolutions, rest, p.pos)
+  }
+
+}
+ */
