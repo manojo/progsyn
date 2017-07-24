46f7c6e02f577b92058b299b904d1ad26fb5f869
manojo <manojo10386@gmail.com>
2017-07-24 11:38:38 +0530

    expand search space to substrings backwards

--- src/main/scala/progsyn/StringExtract.scala
+++ src/main/scala/progsyn/StringExtract.scala
@@ -93,8 +93,55 @@ trait Solution1 extends StringLanguage {
   // genSubstring(List(("hehe", "he"))) will yield both solutions, (0, 2), (2, 4)
 }
 
+/**
+ * Solution 3: growing the space of substrings. We want to be able to refer to
+ * substrings such as "from position (i) till the end"
+ * For this we also have to redefine eval, because the generic `substring` from
+ * java only deals with increasing ranges.
+ */
+trait Solution3 extends StringLanguage {
 
-object StringExtract extends Solution1 {
+  override def eval(e: Exp, s: String): String = e match {
+    case Substring(_, l, r) => funkysub(s, eval(l), eval(r))
+  }
+
+  //returns the empty string if positions don't match
+  //negative indices are used to indicate going backwards
+  // ex: funkysub("hello", -2, -1) should yield "o"
+  def funkysub(s: String, start: Int, end: Int): String = {
+
+    def absidx(k: Int, len: Int): Int = if (k >= 0) k else (len + k + 1)
+    s.slice(absidx(start, s.length), absidx(end, s.length))
+  }
+
+  def genSubstring(examples: List[(String, String)]): Stream[Exp] = {
+    val smallestLen = examples.minBy(pair => pair._1.length)._1.length
+    println(smallestLen)
+
+    genPair(smallestLen)
+      .map { case (st, end) => Substring(StrSym, AbsPos(StrSym, st), AbsPos(StrSym, end)) }
+      .filter(substr => examples.forall { case (in, out) => eval(substr, in) == out })
+  }
+
+  def genPair(maxSize: Int): Stream[(Int, Int)] = {
+    def from(i: Int): Stream[Int] = {
+      if (i > maxSize) Stream.empty
+      else i #:: from(i + 1)
+    }
+
+    def pairsWith(n: Int): Stream[(Int, Int)] =
+      (for (i <- 0 to n) yield (i, n)).toStream
+
+    def pairsWithNeg(n: Int): Stream[(Int, Int)] =
+      (for (i <- (n + 1) to (maxSize + 1)) yield (-i, -n)).toStream
+
+    from(1).flatMap { i => pairsWith(i) #::: pairsWithNeg(i) }
+  }
+
+  // genSubstring(List(("hehe", "he"))) will yield 4 solutions, (-3, -1), (0, 2), (-5, -3), (2, 4)
+}
+
+object StringExtract extends Solution3 {
 
   def main(args: Array[String]): Unit = {
     println("Hi!")
